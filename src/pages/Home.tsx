4d17f3b7631c377b5c831cbabe04aecb9048b258
Justin <j.hebenstreit25@gmail.com>
2024-09-14 19:20:59 -0500

    added more files and CSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import "../CSS/Page.css";
 import "../CSS/Header.css";
-import '../CSS/Carousel.css'
+import '../CSS/Carousel.css';
+import '../CSS/GameAbout.css';
 import Header from "../components/Header";
 import ImageCarousel from "../HomePage/ImageCarousel";
 import Carousel from "../components/ImagesForCarousel";
+import GameInfo from "../HomePage/GameInfo";
 
 export default function Home() {
   return (
     <>
       <div>
         <Header text="Asphalt Legends Unite" />
+        <GameInfo />
         <ImageCarousel project={Carousel} />
       </div>
     </>
