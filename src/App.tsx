cf3c61acb6f017fe9b1ca55bec738cfb153f44aa
Justin <j.hebenstreit25@gmail.com>
2024-09-17 17:31:22 -0500

    refactor prep

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Container from "./components/Container";
+import Header from "./components/Header";
 
 
 export default function App() {
 
   return (
     <>
-      <Outlet />
+    <Header />
+    <Container>
+    <Outlet />
+    </Container>
+      
     </>
   )
 }
