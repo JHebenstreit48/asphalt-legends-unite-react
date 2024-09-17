3a99adba50a9d8de4f8d3cafcd476048f8609239
Justin <j.hebenstreit25@gmail.com>
2024-09-16 22:37:45 -0500

    added files for multiple pages and CSS

--- src/HomePage/GameInfo.tsx
+++ src/HomePage/GameInfo.tsx
@@ -3,13 +3,14 @@ import '../CSS/GameAbout.css';
 export default function GameInfo() {
 return (
 
-<div>
+<div className="asphalt-info">
 
-<p className="asphalt-info">
+<p className='info'>
   Asphalt 9 Legends was first released on July 25th, 2018 and is now called Asphalt Legends Unite as
   of July 17th, 2024. It features a wide variety of cars from manufacturers all over the world. The
   automotive brands featured in this game range from the more common everyday brands like Chevrolet,
   Ford, Nissan, Honda to Luxury and exotic brands such as Lamborghini, Pagani, Ferrari, and much more.
+  When the game was still known as Asphalt 9 Legends, it only had a maximum of 18 garage levels. Since the update and name change to Asphalt Legends Unite, the garage levels have been increased to 60 total garage levels.
 
   <br/>
   <br/>
@@ -20,7 +21,6 @@ return (
 
 </div>
 
-
 )
 
-}
+};
---
+++ src/Manufacturers/AlphabetJumpList.tsx
@@ -0,0 +1,48 @@
+import '../CSS/JumpList.css';
+export default function JumpList() {
+
+return (
+
+<>
+
+<section>
+
+            <div className="brand-alphabetical">
+
+                <nav id="brand-alphabetical">
+
+                    <a href="#a">A</a>
+                    <a href="#b">B</a>
+                    <a href="#c">C</a>
+                    <a href="#d">D</a>
+                    <a href="#e">E</a>
+                    <a href="#f">F</a>
+                    <a href="#g">G</a>
+                    <a href="#h">H</a>
+                    <a href="#i">I</a>
+                    <a href="#j">J</a>
+                    <a href="#k">K</a>
+                    <a href="#l">L</a>
+                    <a href="#m">M</a>
+                    <a href="#n">N</a>
+                    <a href="#p">P</a>
+                    <a href="#r">R</a>
+                    <a href="#s">S</a>
+                    <a href="#t">T</a>
+                    <a href="#u">U</a>
+                    <a href="#v">V</a>
+                    <a href="#w">W</a>
+                    <a href="#z">Z</a>
+
+                </nav>
+
+            </div>
+
+        </section>
+
+</>
+
+)
+
+
+};
