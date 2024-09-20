6b546a2a90e921599e081c168bc28a89c405886e
Justin <j.hebenstreit25@gmail.com>
2024-09-20 12:48:26 -0500

    fixed issue with header and nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Container from "./components/Container";
-import Header from "./components/Header";
 
 
 export default function App() {
 
   return (
     <>
-    <Header />
-    <Container>
+
     <Outlet />
-    </Container>
       
     </>
   )
