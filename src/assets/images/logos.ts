7bfca95ffde0b6cd6e81835f8b27f2d585c46010
Justin <j.hebenstreit25@gmail.com>
2024-09-29 16:47:55 -0500

    worked on setting up Garage Levels

--- src/assets/images/logos.ts
+++ src/assets/images/logos.ts
@@ -1,13 +1,15 @@
 import Acura from '../car-logos/acura-logo.png';
 import Ajlani from '../car-logos/ajlani-logo.png';
 import AlfaRomeo from '../car-logos/alfa-romeo-logo.png';
+import ApexMotors from '../car-logos/apex-motors-logo.png';
+import ApolloAutomobil from '../car-logos/apollo-automobil-logo.png';
 
 export const Logos = {
     Acura,
     Ajlani,
     AlfaRomeo,
-    // ApexMotors,
-    // ApolloAutomobil,
+    ApexMotors,
+    ApolloAutomobil,
 
 
 };
