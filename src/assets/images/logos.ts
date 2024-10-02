ed683754b1503360f6292ee284499a173cadeeff
Justin <j.hebenstreit25@gmail.com>
2024-10-01 22:44:09 -0500

    trying to fix logo deployment issue.

--- src/assets/images/logos.ts
+++ src/assets/images/logos.ts
@@ -1,7 +1,7 @@
 import Acura from '../car-logos/acura-logo.png';
 import Ajlani from '../car-logos/ajlani-logo.png';
 import AlfaRomeo from '../car-logos/alfa-romeo-logo.png';
-import ApexMotors from '../car-logos/apex-motors-logo-2.png';
+import ApexMotors from '../car-logos/apex-motors-logo.png';
 import ApolloAutomobil from '../car-logos/apollo-automobil-logo.png';
 
 export const Logos = {
