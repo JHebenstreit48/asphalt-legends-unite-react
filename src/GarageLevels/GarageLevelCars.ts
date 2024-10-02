eb0e42cace278f54b5e13798d1cc1a2ac3e8b35a
Justin <j.hebenstreit25@gmail.com>
2024-10-01 19:55:02 -0500

    added code to Garage Levels

--- src/GarageLevels/GarageLevelCars.ts
+++ src/GarageLevels/GarageLevelCars.ts
@@ -17,8 +17,11 @@ export const garageLevelList: GarageLevels[] = [
       {
         img: Images.MitsubishiLancerEvo,
         name: "Mitsubishi Lancer Evo"
+      },
+      {
+        img: Images.DodgeChallengerSRT8,
+        name: "Dodge Challenger SRT8",
       }
-
     ],
   },
   {
@@ -26,22 +29,90 @@ export const garageLevelList: GarageLevels[] = [
     xp: 4000,
     garageLevel: [
       {
-        img: Images.BMWZ4,
-        name: "BMW Z4 LCI E89",
+        img: Images.BMW3CSLHommage,
+        name: "BMW 3.0 CSL Hommage",
       },
       {
-        img: Images.ChevroletCamaroLT,
-        name: "Chevrolet Camaro LT",
+        img: Images.ChevroletCamaroZL150TH,
+        name: "Chevrolet Camaro ZL1 50TH Ed.",
       },
       {
-        img: Images.BMWZ4,
-        name: "BMW Z4 LCI E89",
+        img: Images.Nissan370ZNismo,
+        name: "Nissan 370z Nismo",
+      },
+      {
+        img: Images.DSETense,
+        name: "DS E-Tense",
+      },
+    ],
+  },
+  {
+    GarageLevelKey: 3,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.DodgeChallenger392HemiScatPk,
+        name: "Dodge Challenger 392 Hemi Scat Pack",
+      },
+      {
+        img: Images.VWXLSportConcept,
+        name: "Volkswagen XL Sport Concept",
+      },
+      {
+        img: Images.LotusEvoraSport410,
+        name: "Lotus Evora Sport 410",
+      },
+      {
+        img: Images.Porsche911Targa4S,
+        name: "Porsche 911 Targa 4S",
       },
 
     ],
   },
   {
-    GarageLevelKey: 3,
+    GarageLevelKey: 4,
+    xp: 20000,
+    garageLevel: [
+      {
+        img: Images.MercedesBenzAMGGTS,
+        name: "Mercedes-Benz AMG GT S",
+      },
+      {
+        img: Images.Porsche718Cayman,
+        name: "Porsche 718 Cayman",
+      },
+      {
+        img: Images.Porsche911GTSCoupé,
+        name: "Porsche 911 GTS Coupé"
+      },
+      {
+        img: Images.GinettaG60,
+        name: "Ginetta G60"
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 5,
+    xp: 40000,
+    garageLevel: [
+      {
+        img: Images.JaguarFTypeSVR,
+        name: "Jaguar F-Type SVR",
+      },
+      {
+        img: Images.AstonMartinDB11,
+        name: "Aston Martin DB11",
+      },
+      {
+        img: Images.LotusEliseSprint220,
+        name: "Lotus Elise Sprint 220",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 6,
     xp: 8000,
     garageLevel: [
       {
@@ -55,4 +126,815 @@ export const garageLevelList: GarageLevels[] = [
 
     ],
   },
+  {
+    GarageLevelKey: 7,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 8,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 9,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 10,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 11,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 12,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 13,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 14,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 15,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 16,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 17,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 18,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 19,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 20,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 21,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 22,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 23,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 24,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 25,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 26,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 27,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 28,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 29,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 30,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 31,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 32,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 33,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 34,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 35,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 36,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 37,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 38,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 39,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 40,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 41,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 42,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 43,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 44,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 45,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 46,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 47,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 48,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 49,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 50,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 51,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 52,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 53,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 54,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 55,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 56,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 57,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 58,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 59,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+  {
+    GarageLevelKey: 60,
+    xp: 8000,
+    garageLevel: [
+      {
+        img: Images.BMWZ4,
+        name: "BMW Z4 LCI E89",
+      },
+      {
+        img: Images.ChevroletCamaroLT,
+        name: "Chevrolet Camaro LT",
+      },
+
+    ],
+  },
+
 ];
--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,37 +1,140 @@
-import ApexAP0 from '../car-photos/apex-ap-0.jpg';
-import BMWZ4 from '../car-photos/bmw-z4.jpg';
-import ChevroletCamaroLT from '../car-photos/camaro-lt.jpg';
-import LamborghiniSian from '../car-photos/lamborghini-sian-fkp-37.jpg';
-import MitsubishiLancerEvo from '../car-photos/mitsubishi-lancer.jpg'
-import AstonMartinValhalla from '../car-photos/aston-martin-valhalla-concept-car.jpg';
-import PaganiHuayraBC from '../car-photos/pagani-huayra-bc.jpg';
-import LamborghiniSestoElemento from '../car-photos/lamborghini-sesto-elemento.jpg';
-import FerrariFXXK from '../car-photos/ferrari-fxx-k.jpg';
-import VandaElectricsDendrobium from '../car-photos/vanda-electrics-dendrobium.jpg';
-import Porsche911 from '../car-photos/porsche-911-carrera-rs-3-8.jpg';
-import BugattiChiron from '../car-photos/bugatti-chiron.jpg';
-import McLarenP1 from '../car-photos/mclaren-p1.jpg';
-import KoenigseggRegera from '../car-photos/koenigsegg-regera.jpg';
-import RimacConceptOne from '../car-photos/rimac-concept-one.jpg';
-import FerrariLaFerrari from '../car-photos/ferrari-laferrari.jpg';
+import ApexAP0 from "../car-photos/apex-ap-0.jpg";
 
+import AstonMartinDB11 from "../car-photos/aston-martin-db11.jpg";
+
+import AstonMartinValhalla from "../car-photos/aston-martin-valhalla-concept-car.jpg";
+
+import BMW3CSLHommage from "../car-photos/bmw-3-csl.jpg";
+
+import BMWZ4 from "../car-photos/bmw-z4.jpg";
+
+import BugattiChiron from "../car-photos/bugatti-chiron.jpg";
+
+import ChevroletCamaroLT from "../car-photos/camaro-lt.jpg";
+
+import ChevroletCamaroZL150TH from "../car-photos/camaro-zl1-50th.jpg";
+
+import DodgeChallenger392HemiScatPk from "../car-photos/dodge-challenger-392-hemi-scat.jpg";
+
+import DodgeChallengerSRT8 from "../car-photos/dodge-challenger-srt8.jpg";
+
+import DSETense from "../car-photos/ds-e-tense.jpg";
+
+import FerrariFXXK from "../car-photos/ferrari-fxx-k.jpg";
+
+import FerrariLaFerrari from "../car-photos/ferrari-laferrari.jpg";
+
+import JaguarFTypeSVR from "../car-photos/jaguar-f-type-svr.jpg";
+
+import GinettaG60 from "../car-photos/ginetta-g60.jpg";
+
+import KoenigseggRegera from "../car-photos/koenigsegg-regera.jpg";
+
+import LamborghiniSestoElemento from "../car-photos/lamborghini-sesto-elemento.jpg";
+
+import LamborghiniSian from "../car-photos/lamborghini-sian-fkp-37.jpg";
+
+import LotusEliseSprint220 from "../car-photos/lotus-elise-sprint-220.jpg";
+
+import LotusEvoraSport410 from "../car-photos/lotus-evora-sport-410.jpg";
+
+import McLarenP1 from "../car-photos/mclaren-p1.jpg";
+
+import MercedesBenzAMGGTS from "../car-photos/mercedes-benz-amg-gt-s.jpg";
+
+import MitsubishiLancerEvo from "../car-photos/mitsubishi-lancer.jpg";
+
+import Nissan370ZNismo from "../car-photos/nissan-370z-nismo.jpg";
+
+import PaganiHuayraBC from "../car-photos/pagani-huayra-bc.jpg";
+
+import Porsche718Cayman from "../car-photos/porsche-718-cayman.jpg";
+
+import Porsche911 from "../car-photos/porsche-911-carrera-rs-3-8.jpg";
+
+import Porsche911GTSCoupé from "../car-photos/porsche-911-gts-coupe.jpg";
+
+import Porsche911Targa4S from "../car-photos/porsche-911-targa-4s.jpg";
+
+import RimacConceptOne from "../car-photos/rimac-concept-one.jpg";
+
+import VandaElectricsDendrobium from "../car-photos/vanda-electrics-dendrobium.jpg";
+
+import VWXLSportConcept from "../car-photos/volkswagen-xl-sport-concept.jpg";
 
 export const Images = {
-    ApexAP0,
-    BMWZ4,
-    ChevroletCamaroLT,
-    MitsubishiLancerEvo,
-    LamborghiniSian,
-    AstonMartinValhalla,
-    PaganiHuayraBC,
-    LamborghiniSestoElemento,
-    FerrariFXXK,
-    VandaElectricsDendrobium,
-    Porsche911,
-    BugattiChiron,
-    McLarenP1,
-    KoenigseggRegera,
-    RimacConceptOne,
-    FerrariLaFerrari,
-    
+  ApexAP0,
+
+  AstonMartinDB11,
+
+  AstonMartinValhalla,
+
+  BMW3CSLHommage,
+
+  BMWZ4,
+
+  BugattiChiron,
+
+  ChevroletCamaroLT,
+
+  ChevroletCamaroZL150TH,
+
+  DodgeChallenger392HemiScatPk,
+
+  DodgeChallengerSRT8,
+
+  DSETense,
+
+  FerrariFXXK,
+
+  FerrariLaFerrari,
+
+  GinettaG60,
+
+  JaguarFTypeSVR,
+
+  KoenigseggRegera,
+
+  LamborghiniSestoElemento,
+
+  LamborghiniSian,
+
+  LotusEliseSprint220,
+
+  LotusEvoraSport410,
+
+  McLarenP1,
+
+  MercedesBenzAMGGTS,
+
+  MitsubishiLancerEvo,
+
+  Nissan370ZNismo,
+
+  PaganiHuayraBC,
+
+  Porsche718Cayman,
+
+  Porsche911,
+
+  Porsche911GTSCoupé,
+
+  Porsche911Targa4S,
+
+  RimacConceptOne,
+
+  VandaElectricsDendrobium,
+
+  VWXLSportConcept,
+
+  
+
+  
+
+  
+
+  
+
+ 
+
 };
