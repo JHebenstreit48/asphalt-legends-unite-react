4f0d78d90ab2a076ad7cf906e7814cc238bbe94b
Justin <j.hebenstreit25@gmail.com>
2024-10-01 22:39:30 -0500

    added garage levels code and images

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,10 +1,18 @@
+import AcuraNSX from "../car-photos/2017-acura-nsx.jpg";
+
 import ApexAP0 from "../car-photos/apex-ap-0.jpg";
+
+import ArtegaScaloSuperelletra from "../car-photos/artega-scalo-superelletra.jpg";
 
 import AstonMartinDB11 from "../car-photos/aston-martin-db11.jpg";
 
 import AstonMartinValhalla from "../car-photos/aston-martin-valhalla-concept-car.jpg";
 
+import AstonMartinVulcan from "../car-photos/aston-martin-vulcan.jpg";
+
 import BMW3CSLHommage from "../car-photos/bmw-3-csl.jpg";
+
+import BMWM4GTS from "../car-photos/bmw-m4-gts.jpg";
 
 import BMWZ4 from "../car-photos/bmw-z4.jpg";
 
@@ -14,21 +22,49 @@ import ChevroletCamaroLT from "../car-photos/camaro-lt.jpg";
 
 import ChevroletCamaroZL150TH from "../car-photos/camaro-zl1-50th.jpg";
 
+import ChevroletCorvetteGrandSport from "../car-photos/chevrolet-corvette-grand-sport.jpg";
+
 import DodgeChallenger392HemiScatPk from "../car-photos/dodge-challenger-392-hemi-scat.jpg";
 
 import DodgeChallengerSRT8 from "../car-photos/dodge-challenger-srt8.jpg";
 
+import DodgeViperACR from "../car-photos/dodge-viper-acr.jpg";
+
+import DodgeViperGTS from "../car-photos/dodge-viper-gts.jpg";
+
 import DSETense from "../car-photos/ds-e-tense.jpg";
+
+import ExoticRidesW70 from "../car-photos/exotic-rides-w70.jpg";
+
+import Ferrari488GTB from "../car-photos/ferrari-488-gtb.jpg";
+
+import FerrariF12TDF from "../car-photos/ferrari-f12tdf.jpg";
 
 import FerrariFXXK from "../car-photos/ferrari-fxx-k.jpg";
 
+import FerrariJ50 from "../car-photos/ferrari-j50.jpg";
+
 import FerrariLaFerrari from "../car-photos/ferrari-laferrari.jpg";
 
-import JaguarFTypeSVR from "../car-photos/jaguar-f-type-svr.jpg";
+import FordGT from "../car-photos/ford-gt.jpg";
+
+import FordShelbyGT350R from "../car-photos/ford-shelby gt350r.jpg";
 
 import GinettaG60 from "../car-photos/ginetta-g60.jpg";
 
+import Glickenhaus003S from "../car-photos/glickenhaus-003s.jpg";
+
+import IconaVulcanoTitanium from "../car-photos/icona-vulcano-titanium.jpg";
+
+import ItaldesignZerouno from "../car-photos/italdesign-zerouno.jpg";
+
+import JaguarFTypeSVR from "../car-photos/jaguar-f-type-svr.jpg";
+
 import KoenigseggRegera from "../car-photos/koenigsegg-regera.jpg";
+
+import LamborghiniAventadorSVCoupe from "../car-photos/lamborghini-aventador-sv-coupe.jpg";
+
+import LamborghiniCentenario from "../car-photos/lamborghini-centenario.jpg";
 
 import LamborghiniSestoElemento from "../car-photos/lamborghini-sesto-elemento.jpg";
 
@@ -38,6 +74,10 @@ import LotusEliseSprint220 from "../car-photos/lotus-elise-sprint-220.jpg";
 
 import LotusEvoraSport410 from "../car-photos/lotus-evora-sport-410.jpg";
 
+import MaseratiAlfieri from "../car-photos/maserati-alfieri.jpg";
+
+import MazdaFurai from "../car-photos/mazda-furai.jpg";
+
 import McLarenP1 from "../car-photos/mclaren-p1.jpg";
 
 import MercedesBenzAMGGTS from "../car-photos/mercedes-benz-amg-gt-s.jpg";
@@ -46,7 +86,11 @@ import MitsubishiLancerEvo from "../car-photos/mitsubishi-lancer.jpg";
 
 import Nissan370ZNismo from "../car-photos/nissan-370z-nismo.jpg";
 
+import NissanGTRNismo from "../car-photos/nissan-gt-r-nismo.jpg";
+
 import PaganiHuayraBC from "../car-photos/pagani-huayra-bc.jpg";
+
+import PininfarinaH2Speed from "../car-photos/pininfarina-h2-speed.jpg";
 
 import Porsche718Cayman from "../car-photos/porsche-718-cayman.jpg";
 
@@ -56,20 +100,34 @@ import Porsche911GTSCoupé from "../car-photos/porsche-911-gts-coupe.jpg";
 
 import Porsche911Targa4S from "../car-photos/porsche-911-targa-4s.jpg";
 
+import Porsche918Spyder from "../car-photos/porsche-918-spyder.jpg";
+
 import RimacConceptOne from "../car-photos/rimac-concept-one.jpg";
 
+import TVRGriffith from "../car-photos/tvr-griffith.jpg";
+
 import VandaElectricsDendrobium from "../car-photos/vanda-electrics-dendrobium.jpg";
+
+import VencerSarthe from "../car-photos/vencer-sarthe.jpg";
 
 import VWXLSportConcept from "../car-photos/volkswagen-xl-sport-concept.jpg";
 
 export const Images = {
+  AcuraNSX,
+
   ApexAP0,
+
+  ArtegaScaloSuperelletra,
 
   AstonMartinDB11,
 
   AstonMartinValhalla,
 
+  AstonMartinVulcan,
+
   BMW3CSLHommage,
+
+  BMWM4GTS,
 
   BMWZ4,
 
@@ -79,21 +137,49 @@ export const Images = {
 
   ChevroletCamaroZL150TH,
 
+  ChevroletCorvetteGrandSport,
+
   DodgeChallenger392HemiScatPk,
 
   DodgeChallengerSRT8,
 
+  DodgeViperACR,
+
+  DodgeViperGTS,
+
   DSETense,
+
+  ExoticRidesW70,
+
+  Ferrari488GTB,
+
+  FerrariF12TDF,
 
   FerrariFXXK,
 
+  FerrariJ50,
+
   FerrariLaFerrari,
 
+  FordGT,
+
+  FordShelbyGT350R,
+
   GinettaG60,
+
+  Glickenhaus003S,
+
+  IconaVulcanoTitanium,
+
+  ItaldesignZerouno,
 
   JaguarFTypeSVR,
 
   KoenigseggRegera,
+
+  LamborghiniAventadorSVCoupe,
+
+  LamborghiniCentenario,
 
   LamborghiniSestoElemento,
 
@@ -103,6 +189,10 @@ export const Images = {
 
   LotusEvoraSport410,
 
+  MaseratiAlfieri,
+
+  MazdaFurai,
+
   McLarenP1,
 
   MercedesBenzAMGGTS,
@@ -111,7 +201,11 @@ export const Images = {
 
   Nissan370ZNismo,
 
+  NissanGTRNismo,
+
   PaganiHuayraBC,
+
+  PininfarinaH2Speed,
 
   Porsche718Cayman,
 
@@ -121,20 +215,15 @@ export const Images = {
 
   Porsche911Targa4S,
 
+  Porsche918Spyder,
+
   RimacConceptOne,
+
+  TVRGriffith,
 
   VandaElectricsDendrobium,
 
+  VencerSarthe,
+
   VWXLSportConcept,
-
-  
-
-  
-
-  
-
-  
-
- 
-
 };
