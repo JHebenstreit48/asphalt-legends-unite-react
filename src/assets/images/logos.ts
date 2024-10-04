dc13c0f58034216c09d7cc509cd59e9692375107
Justin <j.hebenstreit25@gmail.com>
2024-10-03 19:14:50 -0500

    added more manufacturer info

--- src/assets/images/logos.ts
+++ src/assets/images/logos.ts
@@ -3,6 +3,8 @@ import Ajlani from '../car-logos/ajlani-logo.png';
 import AlfaRomeo from '../car-logos/alfa-romeo-logo.png';
 import ApexMotors from '../car-logos/apex-motors-logo.png';
 import ApolloAutomobil from '../car-logos/apollo-automobil-logo.png';
+import ArashMotorCompanyLtd from '../car-logos/arash-motor-company-ltd-logo.png';
+import AresModena from '../car-logos/ares-modena-logo.png';
 
 export const Logos = {
     Acura,
@@ -10,6 +12,8 @@ export const Logos = {
     AlfaRomeo,
     ApexMotors,
     ApolloAutomobil,
+    ArashMotorCompanyLtd,
+    AresModena,
 
 
 };
