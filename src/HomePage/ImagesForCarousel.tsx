337cacdae6af643619dbcea263d733eaab6a66ee
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2024-10-21 20:31:32 -0500

    added manufacturer logos and images to home page (#38)

--- src/HomePage/ImagesForCarousel.tsx
+++ src/HomePage/ImagesForCarousel.tsx
@@ -5,31 +5,25 @@ export type ImageCarouselType = {
 };
 
 const Carousel: ImageCarouselType[] = [
-    { path: Images.ApexAP0 },
-
-    { path: Images.LamborghiniSianFKP37 },
-
-    { path: Images.AstonMartinValhallaConcept },
 
     { path: Images.PaganiHuayraBC },
-
+    { path: Images.LamborghiniSianFKP37 },
+    { path: Images.ApexAP0 },
+    { path: Images.AstonMartinValhallaConcept },
     { path: Images.LamborghiniSestoElemento },
-
     { path: Images.FerrariFXXK },
-
     { path: Images.VandaElectricsDendrobium },
-
     { path: Images.Porsche911 },
-
     { path: Images.BugattiChiron },
-
     { path: Images.McLarenP1 },
-
     { path: Images.KoenigseggRegera },
-
     { path: Images.RimacConceptOne },
-
     { path: Images.FerrariLaFerrari },
+    { path: Images.LamborghiniCentenario },
+    { path: Images.AstonMartinValkyrie },
+    { path: Images.AutomobiliPininfarinaBattista },
+    { path: Images.BugattiVeyron164GSVitesse },
+
 ];
 
 export default Carousel;
