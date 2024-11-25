f884c57dac219cac25373bd8bf5ac2b2cc0180fc
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2024-11-25 17:28:52 -0600

    Server work (#47)
    * setup server for Cars By Class
    
    * working on getting subpages to render
    
    * fixed README issue
    
    * moved README to root
    
    * working on debugging communication between front and back end
    
    * fixed some server side and client bugs

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,14 +9,13 @@ const app = express();
 app.use(express.json()); // Parse JSON requests
 app.use(cors()); // Enable CORS for cross-origin requests
 
+app.use("/api", apiRoutes);
+
 const main = async () => {
     try {
         await connectToDb(); // Connect to MongoDB
         console.log("Database connected successfully.");
-
-        // Register API routes
-        app.use("/api", apiRoutes);
-
+        
         // Start the server
         const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
