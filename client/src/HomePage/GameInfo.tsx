4832b26f2dcf75ddd3ebdb652de5e0988e538d60
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2024-12-04 14:35:13 -0600

    media queries part 1

--- client/src/HomePage/GameInfo.tsx
+++ client/src/HomePage/GameInfo.tsx
@@ -1,25 +1,30 @@
 import '../CSS/Home.css';
 
 export default function GameInfo() {
-return (
+  return (
 
-<div className="asphalt-info">
+    <div className="asphalt-info">
 
-<p className='info'>
-  Asphalt 9 Legends was first released on July 25th, 2018 and is now called Asphalt Legends Unite as
-  of July 17th, 2024. It features a wide variety of cars from manufacturers all over the world. The
-  automotive brands featured in this game range from the more common everyday brands like Chevrolet,
-  Ford, Nissan, Honda to Luxury and exotic brands such as Lamborghini, Pagani, Ferrari, and much more.
-  When the game was still known as Asphalt 9 Legends, it only had a maximum of 18 garage levels. Since the update and name change to Asphalt Legends Unite, the garage levels have been increased to 60 total garage levels.
+      <div className='info'>
 
-  <br/>
-  <br/>
+        <p>
+          Asphalt 9 Legends was first released on July 25th, 2018 and is now called Asphalt Legends Unite as
+          of July 17th, 2024. It features a wide variety of cars from manufacturers all over the world. The
+          automotive brands featured in this game range from the more common everyday brands like Chevrolet,
+          Ford, Nissan, Honda to Luxury and exotic brands such as Lamborghini, Pagani, Ferrari, and much more.
+          When the game was still known as Asphalt 9 Legends, it only had a maximum of 18 garage levels. Since the update and name change to Asphalt Legends Unite, the garage levels have been increased to 60 total garage levels.
+        </p>
 
-  Asphalt Legends Unite is available on Xbox One and Xbox Series consoles, Playstation 4 & 5, Steam,
-  Windows, Nintendo Switch, iOS, Android, Huawei App Gallery, and Galaxy Store.
-</p>
+        <p>
 
-</div>
+          Asphalt Legends Unite is available on Xbox One and Xbox Series consoles, Playstation 4 & 5, Steam,
+          Windows, Nintendo Switch, iOS, Android, Huawei App Gallery, and Galaxy Store.
+
+        </p>
+
+      </div>
+
+    </div>
 
 )
 
